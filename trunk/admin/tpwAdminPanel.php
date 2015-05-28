3e37d1c816c5d08e0a8faa7fac575ffb3f5c5d2e
Sem Goedknegt <sem@ivaldi.nl>
2015-05-28 12:20:14 +0200

    fix error with wrong admin panel link, update dutch translation

--- trunk/admin/tpwAdminPanel.php
+++ trunk/admin/tpwAdminPanel.php
@@ -7,27 +7,27 @@
 
 class tpwAdminPanel{
 
-    function __construct() {
-        //register administration menu hook
-        add_action( 'admin_menu', array( $this, 'addMenuLink' ) );
-    }
+	function __construct() {
+		//register administration menu hook
+		add_action( 'admin_menu', array( $this, 'addMenuLink' ) );
+	}
 
-    /**
-     * Include the link to the class settings in the menu
-     */
-    function addMenuLink() {
-        add_options_page(
-                __( 'ThePerfectWedding.nl Widget', 'tpwratingwidget' ),
-                __( 'TPW Widget Options', 'tpwratingwidget' ),
-                'administrator',
-                 __( 'TPWWidget', 'tpwratingwidget' ),
-                array( $this, 'renderTemplate' )
-        );
-    }
+	/**
+	 * Include the link to the class settings in the menu
+	 */
+	function addMenuLink() {
+		add_options_page(
+				__( 'ThePerfectWedding.nl Widget', 'tpwratingwidget' ),
+				__( 'TPW Widget Options', 'tpwratingwidget' ),
+				'administrator',
+				'TPWWidget',
+				array( $this, 'renderTemplate' )
+		);
+	}
 
-    function renderTemplate() {
-        include( 'tpwAdminPanelTemplate.php' );
-    }
+	function renderTemplate() {
+		include( 'tpwAdminPanelTemplate.php' );
+	}
 
 }
 
